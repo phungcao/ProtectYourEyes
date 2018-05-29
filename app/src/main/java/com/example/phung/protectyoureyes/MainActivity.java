31dcfcbe36a8c68aaacf05b821d602b57ef94fb7
phungcao <phungcao98515@gmail.com>
2018-05-29 15:23:05 +0700

    setting function for button

--- app/src/main/java/com/example/phung/protectyoureyes/MainActivity.java
+++ app/src/main/java/com/example/phung/protectyoureyes/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.phung.protectyoureyes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button change;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        change = (Button) findViewById(R.id.btOn);
+        final Intent intent = new Intent(this,Time.class);
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
